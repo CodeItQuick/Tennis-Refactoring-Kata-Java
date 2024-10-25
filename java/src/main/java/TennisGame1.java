0d397a5216a52946822999668665a5726add2746
Lada Kesseler <archie.kesseler@gmail.com>
2024-10-25 10:55:47 -0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:java/src/main/java/TennisGame1.java

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -27,30 +27,19 @@ public class TennisGame1 implements TennisGame {
             return scoreForAdvantageOrWin(m_score1, m_score2);
         }
 
-        return regularScore(m_score1, m_score2);
+        return regularScore();
     }
 
-    private static String regularScore(int m_score1, int m_score2) {
-        String score = "";
-
-        score = regularScore(m_score1, score);
-        score += "-";
-        score = regularScore(m_score2, score);
-        return score;
-    }
-
-    private static String regularScore(int m_score1, String score) {
-        String playerScore = switch (m_score1) {
-            case 0 -> "Love";
-            case 1 -> "Fifteen";
-            case 2 -> "Thirty";
-            case 3 -> "Forty";
-            default -> "";
+    private String scoreForTied() {
+        return switch (m_score1) {
+            case 0 -> "Love-All";
+            case 1 -> "Fifteen-All";
+            case 2 -> "Thirty-All";
+            default -> "Deuce";
         };
-        return score + playerScore;
     }
 
-    private static String scoreForAdvantageOrWin(int m_score1, int m_score2) {
+    private String scoreForAdvantageOrWin(int m_score1, int m_score2) {
         String score;
         int minusResult = m_score1 - m_score2;
         if (minusResult == 1) score = "Advantage player1";
@@ -60,12 +49,17 @@ public class TennisGame1 implements TennisGame {
         return score;
     }
 
-    private String scoreForTied() {
-        return switch (m_score1) {
-            case 0 -> "Love-All";
-            case 1 -> "Fifteen-All";
-            case 2 -> "Thirty-All";
-            default -> "Deuce";
+    private String regularScore() {
+        return nicerRegularScore(m_score1) + "-" + nicerRegularScore(m_score2);
+    }
+
+    private String nicerRegularScore(int playerScore) {
+        return switch (playerScore) {
+            case 0 -> "Love";
+            case 1 -> "Fifteen";
+            case 2 -> "Thirty";
+            case 3 -> "Forty";
+            default -> "";
         };
     }
 }
