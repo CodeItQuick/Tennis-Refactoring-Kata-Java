99f899e3daca6b29ee44029d0252583fdf2b0bec
Lada Kesseler <archie.kesseler@gmail.com>
2024-10-25 10:15:38 -0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:java/src/main/java/TennisGame1.java

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -19,39 +19,51 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore = 0;
         if (m_score1 == m_score2) {
-            score = assignScore();
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            int minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
-        } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = m_score1;
-                else {
-                    score += "-";
-                    tempScore = m_score2;
-                }
-                switch (tempScore) {
-                    case 0:
-                        score += "Love";
-                        break;
-                    case 1:
-                        score += "Fifteen";
-                        break;
-                    case 2:
-                        score += "Thirty";
-                        break;
-                    case 3:
-                        score += "Forty";
-                        break;
-                }
+            return assignScore();
+        }
+
+        if (m_score1 >= 4 || m_score2 >= 4) {
+            return assignPlayerAdvantage(m_score1, m_score2);
+        }
+
+        return assignRegularScore(m_score1, m_score2);
+    }
+
+    private static String assignRegularScore(int m_score1, int m_score2) {
+        String score = "";
+        int tempScore;
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) tempScore = m_score1;
+            else {
+                score += "-";
+                tempScore = m_score2;
+            }
+            switch (tempScore) {
+                case 0:
+                    score += "Love";
+                    break;
+                case 1:
+                    score += "Fifteen";
+                    break;
+                case 2:
+                    score += "Thirty";
+                    break;
+                case 3:
+                    score += "Forty";
+                    break;
             }
         }
+        return score;
+    }
+
+    private static String assignPlayerAdvantage(int m_score1, int m_score2) {
+        String score;
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1) score = "Advantage player1";
+        else if (minusResult == -1) score = "Advantage player2";
+        else if (minusResult >= 2) score = "Win for player1";
+        else score = "Win for player2";
         return score;
     }
 
